dcfabe72a877c6f3051d6b82ff383597d2d6b9fc
Bilal <bilal.j@me.com>
2016-04-12 13:27:57 +1200

    testing github

--- main/java/io/github/xbazzu/lonesafe/Login.java
+++ main/java/io/github/xbazzu/lonesafe/Login.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
-//hello test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
     }
+
+
+    void newFunc() {
+        System.out.print("hello");
+    }
 }
+
