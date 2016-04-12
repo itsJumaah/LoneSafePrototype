44a545b7fe91d3bc724044b03e6a30ecd5e6cbf8
Buzz <buzz@Bilals-MacBook-Pro.local>
2016-04-12 13:10:53 +1200

    First Push
    LoneSafe is an application by The Cunnies to help and provide a safety
    layer for lone workers

---
+++ main/java/io/github/xbazzu/lonesafe/Login.java
@@ -0,0 +1,13 @@
+package io.github.xbazzu.lonesafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
