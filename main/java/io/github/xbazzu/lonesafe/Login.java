178f7205cf14da5600fb126857bb138487bb75a0
Bilal <bilal.j@me.com>
2016-04-13 20:47:23 +1200

    added UI elements

--- main/java/io/github/xbazzu/lonesafe/Login.java
+++ main/java/io/github/xbazzu/lonesafe/Login.java
@@ -5,15 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
-    public Login() {
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        System.out.print("its running!!!");
+
     }
 
 
