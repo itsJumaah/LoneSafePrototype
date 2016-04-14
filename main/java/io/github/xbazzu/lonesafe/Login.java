ba39f00b3c151d4a537a94547dc1c73c9eda7b88
Ashneel Kumar <ashneelak@outlook.com>
2016-04-14 21:30:19 +1200

    Added Info button with Alert to show app info. Basic at this stage

--- main/java/io/github/xbazzu/lonesafe/Login.java
+++ main/java/io/github/xbazzu/lonesafe/Login.java
@@ -1,7 +1,10 @@
 package io.github.xbazzu.lonesafe;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,6 +14,20 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
+    }
+
+    public void showAlert(View view) {
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+        myAlert.setMessage("This application is designed for specific users, please see your manager if need a login, Thanks :)")
+                .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setTitle("Information")
+                .create();
+        myAlert.show();
     }
 
     //ashneel
