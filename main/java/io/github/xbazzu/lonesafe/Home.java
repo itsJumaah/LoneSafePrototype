b60de66bcfd145e60ad918ecabca31ae0fbf7222
Ashneel Kumar <ashneelak@outlook.com>
2016-04-18 23:14:20 +1200

    Implemented SOS button and risklvl select. Idk how to change risk lvl on homepage based on what is selected :/

--- main/java/io/github/xbazzu/lonesafe/Home.java
+++ main/java/io/github/xbazzu/lonesafe/Home.java
@@ -1,11 +1,18 @@
 package io.github.xbazzu.lonesafe;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    private String slected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +20,13 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 
+
         final TextView et_name          = (TextView) findViewById(R.id.et_name);
         final TextView et_username      = (TextView) findViewById(R.id.et_username);
-        final TextView et_level_of_risk = (TextView) findViewById(R.id.et_level_of_risk);
+        final TextView et_level_of_risk = (TextView) findViewById(R.id.RLHere);
 
-        Intent intent = getIntent();
+
+      //  Intent intent = getIntent();
 
        // String name     = intent.getStringExtra("user");
         //String username = "user";
@@ -29,7 +38,32 @@ public class Home extends AppCompatActivity {
 
        // et_name.setText(welcome);
        // et_username.setText(logged);
-       // et_level_of_risk.setText(lvl);
 
     }
+
+    public void showSOSAlert(View view) {
+        AlertDialog.Builder myAlert = new AlertDialog.Builder(this);
+        myAlert.setMessage("You are about to activate an alarm, Are you sure you want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setTitle("Warning!!")
+                .create();
+        myAlert.show();
+    }
+    //Goes to settings page when buton clicked
+    public void GoToSettings (View view) {
+        Intent settings = new Intent(Home.this, Settings.class);
+        Home.this.startActivity(settings);
+    }
+
 }
